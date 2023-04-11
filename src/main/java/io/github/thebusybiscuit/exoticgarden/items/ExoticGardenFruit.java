e4d9ab88727e60ca0832b8ec340c2c5d4ffc5eaf
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2023-04-11 11:11:25 -0700

    more stuff

--- src/main/java/io/github/thebusybiscuit/exoticgarden/items/ExoticGardenFruit.java
+++ src/main/java/io/github/thebusybiscuit/exoticgarden/items/ExoticGardenFruit.java
@@ -73,11 +73,27 @@ public class ExoticGardenFruit extends SimpleSlimefunItem<ItemUseHandler> {
             case HOPPER:
             case TRAPPED_CHEST:
             case ENDER_CHEST:
-            case CAULDRON:
             case SHULKER_BOX:
+            case CRAFTING_TABLE:
+            case FURNACE:
+            case SMOKER:
+            case BLAST_FURNACE:
+            case JIGSAW:
+            case JUKEBOX:
+            case ENCHANTING_TABLE:
+            case ITEM_FRAME:
+            case LOOM:
+            case CARTOGRAPHY_TABLE:
+            case GRINDSTONE:
+            case SMITHING_TABLE:
+            case BELL:
+            case BEACON:
                 return true;
             default:
-                return material.name().equals("BARREL") || material.name().endsWith("_SHULKER_BOX");
+                return material.name().equals("BARREL") ||
+                    material.name().endsWith("_SHULKER_BOX") ||
+                    material.name().endsWith("DOOR") ||
+                    material.name().endsWith("BED");
         }
     }
 
